ca4b9d1b6eac74a3b492a6002dcf091b3f9fb4ae
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-15 14:21:37 +0100

    Add logic for login and registration without JWT

--- web/V1/controllers/user_controller.ex
+++ web/V1/controllers/user_controller.ex
@@ -2,6 +2,7 @@ defmodule DocsUsers.V1.UserController do
   use DocsUsers.Web, :controller
 
   alias DocsUsers.V1.User
+  alias Comeonin.Bcrypt
 
   # TODO: remove this debugging function
   def index(conn, _) do
@@ -20,32 +21,41 @@ defmodule DocsUsers.V1.UserController do
     }
   end
 
-  # TODO: generate real JWT token if login is valid
+  # TODO: use when: is_email or similar
   def login(conn, %{"email" => email, "password" => password}) do
-    # TODO: validate inputs
-    # TODO: check if credentials match
-    # TODO: generate a token if match, else throw
-    # TODO: use guardian db to save the token in the DB
-    json conn, %{
-      success: true,
-      data: %{
-        uuid: "abcd",
-        name: "Roland",
+    # TODO: use guardian & guardian db?
+
+    # check if the given user exists and whether passwords match
+    with %User{id: id, uuid: uuid, email: email, password: hash, active: true} <- Repo.get_by(User, email: email), 
+         true <- Bcrypt.checkpw(password, hash) do
+
+      # return a JSON response with UUID and JWT
+      return_data(conn, %{
+        id: id,
+        uuid: uuid,
         token: "JWT"
-      }
-    }
+      })
+
+    else
+
+      # the user does exist but is inactive, return a failure message
+      %User{email: email, password: password, active: false} -> return_message(conn, "ACCOUNT_INACTIVE")
+        
+      # the user doesn't exist, return a failure message
+      _ -> return_message(conn, "LOGIN_INVALID")
+
+    end 
   end
 
-  # TODO
-  def register(conn, request) do
+  def register(conn, %{"name" => name, "email" => email, "password" => password}) do
     # generate a new user using the model
     changeset = User.changeset(%User{}, %{
       # generate a UUID for the new user
       :uuid => Ecto.UUID.generate,
       # extract request data
-      :name => request["name"],
-      :email => request["email"],
-      :password => request["password"],
+      :name => name,
+      :email => email,
+      :password => Bcrypt.hashpwsalt(password),
       # the new user should be inactive by default
       :active => false 
     })
@@ -55,27 +65,28 @@ defmodule DocsUsers.V1.UserController do
       {:ok, user} ->
         # the user was successfully added
         # return a successful JSON response
-        json conn, %{
-          success: true,
-          data: %{
-            id: user.id,
-            uuid: user.uuid,
-            token: "JWT"
-          },
-          # TODO: remove debug output
-          request: request
-        }
+        return_data(conn, %{
+          id: user.id,
+          uuid: user.uuid,
+          token: "JWT"
+        })
 
       {:error, changeset} ->
         # the user could not be created
         # return a failure JSON response
-        json conn, %{
-          success: false,
-          # TODO: remove debug output
-          request: request,
+        return_error(conn, %{
+          message: "INVALID_BODY",
           errors: changeset.errors
-        }
+        })
     end
+  end
+  
+  def login(conn, _) do
+    invalid conn
+  end
+
+  def register(conn, _) do
+    invalid conn
   end
 
   # TODO: implement using UUID
@@ -102,6 +113,30 @@ defmodule DocsUsers.V1.UserController do
           success: false
         }
     end
+  end
+
+  defp invalid(conn) do
+    return_message(conn, "INVALID_BODY")
+  end
+
+  defp return_data(conn, data_map) do
+    json conn, %{
+      success: true,
+      data: data_map
+    }
+  end
+
+  defp return_error(conn, error_map) do
+    json conn, %{
+      success: false,
+      error: error_map
+    }
+  end
+
+  defp return_message(conn, message_string) do
+    return_error(conn, %{
+      message: message_string
+    })
   end
 
 end
