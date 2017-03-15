dcdb202c16280020f84b49646338d1eba5d11a95
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-15 15:42:58 +0100

    Encode the UUID as sub of the token

--- web/V1/guardian_serializer.ex
+++ web/V1/guardian_serializer.ex
@@ -4,9 +4,9 @@ defmodule DocsUsers.V1.GuardianSerializer do
   alias DocsUsers.Repo
   alias DocsUsers.V1.User
 
-  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(user = %User{}), do: { :ok, "UUID:#{user.uuid}" }
   def for_token(_), do: { :error, "Unknown resource type" }
 
-  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token("UUID:" <> uuid), do: { :ok, Repo.get_by(User, uuid: uuid) }
   def from_token(_), do: { :error, "Unknown resource type" }
 end
