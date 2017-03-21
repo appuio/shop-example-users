ba1a9d7820bb81051ad940cd5d7e380669898ba8
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-21 10:11:43 +0100

    Use latest for local build script

--- start-build.sh
+++ start-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it --rm --volume "/opt/git/services/users:/app" shop-example-users-builder:centos bash # mix clean --deps && mix deps.get && MIX_ENV=prod mix release
+docker run -it --rm --volume "/opt/git/services/users:/app" shop-example-users-builder:latest mix clean --deps && mix deps.get && MIX_ENV=prod mix release
 
