e8fbb8903a64058dee47a3b759b958d58a6b63d7
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-27 16:36:41 +0200

    Update to use new /app/source directory for building

--- start-build.sh
+++ start-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build ../users-builder -t shop-example-users-builder
-docker run -t -e "DB_USERNAME=users" -e "DB_PASSWORD=secret" -e "DB_DATABASE=users" -e "DB_HOSTNAME=users-db" --volume "/opt/git/services/users:/app" shop-example-users-builder /bin/sh -c "mix clean --deps;mix deps.get;PORT=4000 MIX_ENV=prod mix release"
+docker run -t -e "DB_USERNAME=users" -e "DB_PASSWORD=secret" -e "DB_DATABASE=users" -e "DB_HOSTNAME=users-db" --volume "/opt/git/services/users:/app/source" shop-example-users-builder /bin/sh -c "mix clean --deps;mix deps.get;MIX_ENV=prod mix release"
