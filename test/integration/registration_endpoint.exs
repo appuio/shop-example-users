54fe11844ae4a5c4613b04266d43204da2d5ca65
Roland Schläfli <rolandschlaefli@gmail.com>
2017-03-29 17:26:55 +0200

    Add todos for registration tests

--- test/integration/registration_endpoint.exs
+++ test/integration/registration_endpoint.exs
@@ -11,4 +11,31 @@ defmodule RegistrationEndpoint do
     {:ok, conn: conn}
   end
 
+  test "registration with invalid body" do
+    # POST an empty body
+    conn = post build_conn(), "/api/v1/users", %{}
+
+    # make sure that registration was denied and body marked as invalid
+    assert json_response(conn, 200) == %{
+      "success" => false,
+      "error" => %{
+        "message" => "INVALID_BODY"
+      }
+    }
+  end
+
+  test "registration with valid body" do
+    # POST a valid body
+    conn = post build_conn(), "/api/v1/users", %{
+      name: "Tester",
+      email: "tester@gmail.com",
+      password: "abcd"
+    }
+
+    # TODO: verify that user has been added to db
+    # TODO: verify that pw was hashed correctly
+    # TODO: verify that user was set to inactive
+    # TODO: verify that registration returns correct data
+  end
+
 end
