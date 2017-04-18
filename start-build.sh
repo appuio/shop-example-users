549fc5d4295bfa24fdb1113cd9b6b4c2f3525d6f
Roland Schläfli <rolandschlaefli@gmail.com>
2017-04-18 16:39:57 +0200

    Use prebuilt builder for running the build

--- start-build.sh
+++ start-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build ../users-builder -t shop-example-users-builder
-docker run -t --volume "/opt/git/services/users:/app/source" shop-example-users-builder /bin/sh -c "mix clean --deps;mix deps.get;MIX_ENV=prod mix release --env=prod"
+# docker build ../users-builder -t shop-example-users-builder
+docker run -t --volume "/opt/git/services/users:/app/source" appuio/shop-example-users-builder /bin/sh -c "mix clean --deps;mix deps.get;MIX_ENV=prod mix release --env=prod"
