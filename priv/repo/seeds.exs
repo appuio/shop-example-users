2de2530d63de69bb5d18bd8865f8217aa8bc5a67
Roland Schläfli <rolandschlaefli@gmail.com>
2017-04-25 18:30:30 +0200

    (fix) Seed user pw was not hashed correctly

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,4 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-DocsUsers.Repo.insert!(%DocsUsers.V1.User{email: "tester@appuio.ch", password: "abcd", active: true})
+DocsUsers.Repo.insert!(%DocsUsers.V1.User{
+  id: 1,
+  uuid: "1b206337-bd9f-495b-992b-5386ad14d10f",
+  name: "Tester",
+  email: "tester@appuio.ch",
+  password: Comeonin.Bcrypt.hashpwsalt("abcd"),
+  active: true})
