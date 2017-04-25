af30dfcf4097ec8ab073bbee6a3abbdbc371c6ed
Roland Schläfli <rolandschlaefli@gmail.com>
2017-04-25 18:19:50 +0200

    (fix) Seed insert statement

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,4 +9,4 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-DocsUsers.Repo.insert! %User{email: "tester@appuio.ch", password: "abcd", active: true}
+DocsUsers.Repo.insert!(%DocsUsers.V1.User{email: "tester@appuio.ch", password: "abcd", active: true})
